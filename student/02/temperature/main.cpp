297fb66b81086f494c85bb09a6a3e8d969b7221d
Aynur Rahman Talukdar <aynurrahman.talukdar@tuni.fi>
2023-06-03 10:27:13 +0300

    temp exc

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -6,7 +6,14 @@ int main()
 {
     cout << "Enter a temperature: ";
 
-    // Write your code here
+    double temperature = 0;
+    cin >> temperature;
+    double temp_fahrenheit = temperature * 1.8 +32;
+    double temp_celsius = (temperature-32)/1.8;
+
+    cout << temperature << " degrees Celsius is " << temp_fahrenheit << " degrees Fahrenheit" << endl;
+    cout << temperature << " degrees Fahrenheit is " << temp_celsius << " degrees Celsius" << endl;
+
 
     return 0;
 }
